2baf1577b8d08428ba8c9ce8079afc62e4153b9d
badaev52pk1 <badaev52@gmail.com>
2023-01-06 22:01:36 +0300

    Задача решена

--- Program.cs
+++ Program.cs
@@ -7,7 +7,49 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
+Console.WriteLine();
+String[] arrayStr1 = { "hello", "2", "world", ":-)" };
+printArr(arrayStr1);
+Console.Write(" -> ");
+printStr(arrayStr1);
+Console.WriteLine(); Console.WriteLine();
+String[] arrayStr2 = { "1234", "1567", "-2", "computer science" };
+printArr(arrayStr2);
+Console.Write(" -> ");
+printStr(arrayStr2);
+Console.WriteLine(); Console.WriteLine();
+String[] arrayStr3 = { "Russia", "Denmark", "Kazan" };
+printArr(arrayStr3);
+Console.Write(" -> ");
+printStr(arrayStr3);
+Console.WriteLine();
 
-String[] arrayStr = {"hello", "2", "world", ":-)"};
+void printArr(String[] strArr)
+{
 
+    Console.Write("[");
+    for (int i = 0; i < strArr.Length; i++)
+    {
 
+        Console.Write($"\"{strArr[i]}\", ");
+
+    }
+    Console.Write("]");
+}
+
+void printStr(String[] strArr)
+{
+
+    Console.Write("[");
+    bool fl = false;
+    for (int i = 0; i < strArr.Length; i++)
+    {
+        if (strArr[i].Length <= 3)
+        {
+            if(fl) Console.Write(", ");
+            fl = true;
+            Console.Write($"\"{strArr[i]}\"");
+        }
+    }
+    Console.Write("]");
+}
